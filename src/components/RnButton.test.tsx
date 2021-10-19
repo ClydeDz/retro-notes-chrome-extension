e1a3191e0980c09991b076c55b99465b7bb3bced
clydedz <clyd94@gmail.com>
2021-10-19 19:50:05 +1300

    Refactor tests

--- src/components/RnButton.test.tsx
+++ src/components/RnButton.test.tsx
@@ -1,22 +1,21 @@
 import { render, screen } from "@testing-library/react";
 import RnButton from "./RnButton";
 
+const mockOnClick = jest.fn();
+
 describe("<RnButton />", () => {
   test("snapshot testing the default view", () => {
-    const mockOnClick = jest.fn();
     const view = render(<RnButton text="Click me" handleOnClick={mockOnClick} />);
     expect(view).toMatchSnapshot();
   });
 
   test("renders the button", () => {
-    const mockOnClick = jest.fn();
     render(<RnButton text="Click me" handleOnClick={mockOnClick} />);
     const buttonElement: HTMLElement = screen.getByText(/Click me/i);
     expect(buttonElement).toBeInTheDocument();
   });
 
   test("supplied method is triggered when button is clicked", () => {
-    const mockOnClick = jest.fn();
     render(<RnButton text="Click me" handleOnClick={mockOnClick} />);
     screen.getByText(/Click me/i).click();
     expect(mockOnClick).toHaveBeenCalled();
--- src/components/RnTextarea.test.tsx
+++ src/components/RnTextarea.test.tsx
@@ -1,22 +1,21 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import RnTextarea from "./RnTextarea";
 
+const mockOnChange = jest.fn();
+
 describe("<RnTextarea />", () => {
   test("snapshot testing the default view", () => {
-    const mockOnChange = jest.fn();
     const view = render(<RnTextarea text="Test content" handleOnChange={mockOnChange}/>);
     expect(view).toMatchSnapshot();
   });
 
   test("renders the contents of text area", () => {
-    const mockOnChange = jest.fn();
     render(<RnTextarea text="Test content" handleOnChange={mockOnChange}/>);
     const textareaElement: HTMLElement = screen.getByText(/Test content/i);
     expect(textareaElement).toBeInTheDocument();
   });
 
   test("triggers the supplied onchange method when content is updated", async () => {
-    const mockOnChange = jest.fn();
     render(<RnTextarea text="Test content" handleOnChange={mockOnChange}/>);
     const input = screen.getByLabelText("Text area");
     fireEvent.change(input, { target: { value: "23" } });
