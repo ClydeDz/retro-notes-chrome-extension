02cd01293ad53a50ee46821bac3a3cadad2f34e8
clydedz <clyd94@gmail.com>
2021-10-19 19:41:19 +1300

    Refine the tests

--- src/components/RnUserInput.test.tsx
+++ src/components/RnUserInput.test.tsx
@@ -15,123 +15,101 @@ describe("<RnUserInput />", () => {
     expect(textareaElement).toBeInTheDocument();
   });
 
-
   test("chrome.storage.sync.get is called when component is loaded in default view", () => {
     const storageSyncGetSpy = jest.spyOn(chrome.storage.sync, "get");
     render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
-    expect(storageSyncGetSpy).toBeCalled();
+    expect(storageSyncGetSpy).toBeCalledWith(["MySpecialKey"], expect.any(Function));
   });
 
-  describe("Mocking chrome storage sync get", () => {
-    test("renders the component when storage key is found", () => {
-      const response = { MySpecialKey: "I am a magical value" };
-      const key = "MySpecialKey";
-
-      global.chrome = {
-        ... chrome,
-        storage: {
-          sync: {
-            // @ts-expect-error
-            get: function(key, callback) {
-              if(!key) { return };
-              callback(response);
-            },
-          }
-        }
-      };
-
-      render(<RnUserInput text="Click me" componentKey={key} />);
-      const textareaElement: HTMLElement = screen.getByLabelText("Text area");
-      expect(textareaElement.textContent).toBe("I am a magical value");
-    });
-
-    test("renders the component when storage key is not found", () => {
-      const response = { NotFound: "This value never displays" };
-      const key = "MySpecialKey";
-
-      global.chrome = {
-        ... chrome,
-        storage: {
-          sync: {
-            // @ts-expect-error
-            get: function(key, callback) {
-              if(!key) { return };
-              callback(response);
-            },
-          }
-        }
-      };
-
-      render(<RnUserInput text="Click me" componentKey={key} />);
-      const textareaElement: HTMLElement = screen.getByLabelText("Text area");
-      expect(textareaElement.textContent).toBe("");
-    });
+  test("chrome.storage.sync.set is called when component is updated", () => {
+    const storageSyncSetSpy = jest.spyOn(chrome.storage.sync, "set");
+    render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
+    const textareaElement: HTMLElement = screen.getByLabelText("Text area");
+    fireEvent.change(textareaElement, { target: { value: "23" } });
+    expect(storageSyncSetSpy).toBeCalledWith({"MySpecialKey": "23"});
   });
 
-  describe("Mocking chrome storage sync set", () => {
-    test("chrome.storage.sync.set is called when an update is made to the text area", () => {
-      const mockStorageSyncSet = jest.fn();
-  
-      global.chrome = {
-        ... chrome,
-        // @ts-expect-error
-        storage: {
-          ...chrome.storage,
-          sync: {
-            ...chrome.storage.sync,
-            set: mockStorageSyncSet
-          }
-        }
-      };
-  
-      render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
-      const textareaElement: HTMLElement = screen.getByLabelText("Text area");
-      fireEvent.change(textareaElement, { target: { value: "23" } });
-      expect(mockStorageSyncSet).toBeCalled();
-    });
-  
-    test("chrome.storage.sync.set is not called when no update is made to the text area", () => {
-      const mockStorageSyncSet = jest.fn();
-  
-      global.chrome = {
-        ... chrome,
-        // @ts-expect-error
-        storage: {
-          ...chrome.storage,
-          sync: {
-            ...chrome.storage.sync,
-            set: mockStorageSyncSet
-          }
-        }
-      };
-  
-      render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
-      expect(mockStorageSyncSet).not.toBeCalled();
-    });
+  test("chrome.storage.sync.set is not called when component is not updated", () => {
+    const storageSyncSetSpy = jest.spyOn(chrome.storage.sync, "set");
+    render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
+    expect(storageSyncSetSpy).not.toBeCalled();
   });
 
-  describe("Mocking use state", () => {
-    test("get state is called when component is loaded", () => {
-      const setState = jest.fn();
-      const useStateSpy = jest.spyOn(React, "useState");
+  test("renders the component when storage key is found", () => {
+    const response = { MySpecialKey: "I am a magical value" };
+    const key = "MySpecialKey";
+
+    global.chrome = {
+      ... chrome,
+      storage: {
+        sync: {
+          // @ts-expect-error
+          get: function(key, callback) {
+            if(!key) { return };
+            callback(response);
+          },
+        }
+      }
+    };
+
+    render(<RnUserInput text="Click me" componentKey={key} />);
+    const textareaElement: HTMLElement = screen.getByLabelText("Text area");
+    expect(textareaElement.textContent).toBe("I am a magical value");
+  });
+
+  test("renders the component when storage key is not found", () => {
+    const response = { NotFound: "This value never displays" };
+    const key = "MySpecialKey";
+
+    global.chrome = {
+      ... chrome,
+      storage: {
+        sync: {
+          // @ts-expect-error
+          get: function(key, callback) {
+            if(!key) { return };
+            callback(response);
+          },
+        }
+      }
+    };
+
+    render(<RnUserInput text="Click me" componentKey={key} />);
+    const textareaElement: HTMLElement = screen.getByLabelText("Text area");
+    expect(textareaElement.textContent).toBe("");
+  });
+
+  test("set state is called with undefined when component is loaded", () => {
+    const setState = jest.fn();
+    const useStateSpy = jest.spyOn(React, "useState");
+    // @ts-expect-error
+    useStateSpy.mockImplementation(init => [init, setState]);
+
+    render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
+    expect(setState).toBeCalledWith(undefined);
+  });
+
+  test("set state is called with updated value that is made to the text area", () => {
+    const setState = jest.fn();
+    const useStateSpy = jest.spyOn(React, "useState");
+    // @ts-expect-error
+    useStateSpy.mockImplementation(init => [init, setState]);
+
+    global.chrome = {
+      ... chrome,
       // @ts-expect-error
-      useStateSpy.mockImplementation(init => [init, setState]);
+      storage: {
+        sync: {
+          ...chrome.storage.sync,
+          set: jest.fn()
+        }
+      }
+    };
 
-      render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
-      expect(setState).not.toBeCalled();
-    });
-
-    test("set state is called when an update is made to the text area", () => {
-      const setState = jest.fn();
-      const useStateSpy = jest.spyOn(React, "useState")
-      // @ts-expect-error
-      useStateSpy.mockImplementation(init => [init, setState]);
-
-      render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
-      const textareaElement: HTMLElement = screen.getByLabelText("Text area");
-      fireEvent.change(textareaElement, { target: { value: "23" } });
-      expect(setState).toBeCalledWith("23");
-    });
+    render(<RnUserInput text="Click me" componentKey="MySpecialKey" />);
+    const textareaElement: HTMLElement = screen.getByLabelText("Text area");
+    fireEvent.change(textareaElement, { target: { value: "23" } });
+    expect(setState).toBeCalledWith("23");
   });
 });
 
